4dbe55ddca7b1fd9e19c46155c7ab45c655ffa06
Mateo <45690579+c-mateo@users.noreply.github.com>
2025-12-08 18:23:24 -0300

    Use zendriver to interact with the UI

--- Worker.py
+++ Worker.py
@@ -1,7 +1,11 @@
-from chatexchange.client import Client
+import asyncio
+import sys
 import time
-import json
+
+import zendriver as zd
+
 from datetime import datetime, timedelta
+
 
 #Datos de inicio de sesión 
 EMAIL = "usertest@gmail.com"
@@ -16,20 +20,58 @@ HORA = 8
 fecha = datetime.strptime(FECHA, "%d-%m-%Y")
 dias = (datetime.now()- fecha).days
 
-def iniciar():
-    try:        
-        with open("cookies.json", "r") as f:
-            cookie_data = json.load(f)
-        client = Client("stackexchange.com")
-        for c in cookie_data:
-            client._br.session.cookies.set(c["name"], c["value"], domain=c["domain"], path=c["path"])
-        #client.login(EMAIL, PASSWORD)        
-        time.sleep(10)
-        room = client.get_room(ROOM_ID)
-        room.join()
-        room.send_message("Dia " + str(abs(dias)) + " sin Trauma")
-        print("Dia " + str(abs(dias)) + " sin Trauma")
-        #client.logout()         
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
+async def enviar_mensaje():
+    try:
+        browser = await zd.start(headless=True)
+        page = await browser.get("https://chat.stackexchange.com/rooms/30872/stack-overflow-en-espanol")
+        checkbox = await page.wait_for("input#agree-to-code")
+        await checkbox.click()
+
+        await asyncio.sleep(0.5)
+
+        button = await page.wait_for("button#general-rules-button")
+        await button.click()
+
+        await asyncio.sleep(0.5)
+
+        login = await page.wait_for("#bubble > a:nth-child(1)")
+        await login.click()
+
+        await asyncio.sleep(1)
+
+        accept_cookies = await page.wait_for("#onetrust-reject-all-handler")
+        await accept_cookies.click()
+
+        await asyncio.sleep(0.5)
+        
+        email_input = await page.wait_for("#email")
+        await email_input.send_keys(EMAIL)
+
+        await asyncio.sleep(0.5)
+
+        password_input = await page.wait_for("#password")
+        await password_input.send_keys(PASSWORD)
+
+        await asyncio.sleep(0.5)
+
+        submit_button = await page.wait_for("#submit-button")
+        await submit_button.click()
+
+        await asyncio.sleep(5)
+
+        textarea = await page.wait_for("#input")
+        await textarea.send_keys(f"Dia {abs(dias)} sin Trauma")
+
+        await asyncio.sleep(1)
+
+        send_button = await page.wait_for("#sayit-button")
+        await send_button.click()
+
+        await asyncio.sleep(1)
+
         return True
     except Exception as e:
         print("\nError de inicio de sesión.")
@@ -42,8 +84,8 @@ while True:
     
     if ahora >= hora_objetivo_hoy:
     # La ejecución de hoy ya pasó -> programar mañana
-        if iniciar():
-            dias += 1
+        asyncio.run(enviar_mensaje())
+        dias += 1
         proxima_ejecucion = hora_objetivo_hoy + timedelta(days=1)
     else:
         proxima_ejecucion = hora_objetivo_hoy
